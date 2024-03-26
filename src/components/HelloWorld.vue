d2ef20785a1127e66a4d1c3f7b39ef5835a9ba22
Diego Buriti <bs.diego97@gmail.com>
2024-03-26 09:00:20 -0300

    chore: Adding prime vue, prime icons and prime flex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Button from 'primevue/button'
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
@@ -10,7 +11,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <Button @click="count++">count is {{ count }}</Button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {})
+app.mount('#app')
