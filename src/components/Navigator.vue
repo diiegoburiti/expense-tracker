83ac5ef2b3322762e82e4f2d65b41e3792eeac02
Diego Buriti <bs.diego97@gmail.com>
2024-03-30 16:10:19 -0300

    chore: Adding Navigation component

---
+++ src/components/Navigator.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <Menubar :model="navigationItems" class="flex justify-content-between">
+      <template #start>
+        <div class="flex align-items-center gap-2">
+          <i class="pi pi-money-bill" style="font-size: 2rem" />
+          <h3 class="">Expense Tracker</h3>
+        </div>
+      </template>
+      <template #item="{ item, props }">
+        <router-link v-slot="{ href, navigate }" :to="item.route" custom>
+          <a
+            class="flex align-items-center"
+            :href="href"
+            v-bind="props.action"
+            @click="navigate"
+          >
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<script setup>
+import Menubar from 'primevue/menubar'
+import { ref } from 'vue'
+
+const navigationItems = ref([
+  {
+    label: 'Home',
+    route: '/'
+  },
+  {
+    label: 'Login',
+    route: 'login'
+  },
+  {
+    label: 'Sign Up',
+    route: 'sign-up'
+  },
+  {
+    label: 'About Us',
+    route: 'about-us'
+  }
+])
+</script>
