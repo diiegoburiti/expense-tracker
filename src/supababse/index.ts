2ec4f00ed71a473f5f8340627da2a965ef867b38
Diego Buriti <bs.diego97@gmail.com>
2024-05-02 11:01:13 -0300

    chore: Adding typescript types for supabase

--- src/supababse/index.ts
+++ src/supababse/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/types/supabase'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
