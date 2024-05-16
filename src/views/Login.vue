7064437757f5fa0904fa1fa3d8c21417606cf451
Diego Buriti <bs.diego97@gmail.com>
2024-05-16 14:31:20 -0300

    chore: Adding Login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,16 @@
 <template>
   <section
-    class="w-screen h-screen flex justify-content-center align-items-center bg-primary"
+    class="h-screen flex justify-content-center align-items-center bg-primary"
     :style="{
       background: 'linear-gradient(270deg, #4c3387, #8B5CF6)'
     }"
   >
+    <Toast />
     <div class="border-1 surface-border surface-card border-round p-6">
       <div class="flex align-items-center flex-column gap-3 mb-3">
         <div class="w-full mb-2">
           <span class="text-900 text-xl font-bold mb-2 block">Log in</span>
-          <span class="text-400 font-medium"
-            >Please enter your e-mail and password</span
-          >
+          <span class="text-800">Please enter your e-mail and password</span>
         </div>
         <IconField iconPosition="left" class="w-full">
           <InputIcon class="pi pi-envelope"> </InputIcon>
@@ -49,25 +48,47 @@
         type="button"
         label="Log In"
         @click="handleLogIn"
+        :disabled="disableButton"
       ></Button>
     </div>
   </section>
 </template>
 
-<script setup>
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import IconField from 'primevue/iconField'
-import InputIcon from 'primevue/inputIcon'
-import { ref } from 'vue'
-import Checkbox from 'primevue/checkbox'
+<script setup lang="ts">
+import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { supabase } from '@/supababse'
+import { useToast } from 'primevue/usetoast'
 
 const email = ref('')
 const password = ref('')
 const remember = ref('')
 const router = useRouter()
-const handleLogIn = () => {
-  router.push({ name: 'home' })
+
+const toast = useToast()
+
+const disableButton = computed(() => !email.value || !password.value)
+
+const handleLogIn = async () => {
+  try {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value
+    })
+
+    if (error) {
+      return toast.add({
+        severity: 'error',
+        summary: 'Ops!',
+        detail: error.message,
+        life: 3000
+      })
+    }
+    router.push({ name: 'home' })
+  } catch (error) {
+    console.error(error)
+  }
 }
 </script>
+
+<style></style>
