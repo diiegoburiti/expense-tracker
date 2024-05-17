56ffa260615e48b19e24900e8dfbdf7cd7c53249
Diego Buriti <bs.diego97@gmail.com>
2024-05-17 13:09:38 -0300

    chore: Adding logic for protected routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/Login.vue'
 import HomeView from '@/views/Home.vue'
+import LoginView from '@/views/Login.vue'
+import AccountDetail from '@/views/AccountDetail.vue'
+import { supabase } from '@/supababse'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/login', name: 'login', component: LoginView },
-    { path: '/', name: 'home', component: HomeView }
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/expense/:id', name: 'account-detail', component: AccountDetail }
   ]
+})
+
+router.beforeEach(async (to, from, next) => {
+  const { data } = await supabase.auth.getSession()
+
+  if (to.name !== 'login' && !data.session) next({ name: 'login' })
+  else next()
 })
 
 export default router
