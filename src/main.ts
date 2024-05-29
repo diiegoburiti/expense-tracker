0adeb146ff61dfacc58d565f12d4f9dfef64fbc2
Diego Buriti <bs.diego97@gmail.com>
2024-05-29 17:59:10 -0300

    chore: Adding config to autoimport primevue components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,47 @@
+/* eslint-disable vue/no-reserved-component-names */
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
+import Dropdown from 'primevue/dropdown'
+import InputNumber from 'primevue/inputnumber'
+import Menubar from 'primevue/menubar'
+import IconField from 'primevue/iconField'
+import InputIcon from 'primevue/inputIcon'
+import Checkbox from 'primevue/checkbox'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+import Card from 'primevue/card'
+import Skeleton from 'primevue/skeleton'
+import Calendar from 'primevue/calendar'
+
 import App from './App.vue'
-import 'primevue/resources/themes/aura-light-green/theme.css'
+import router from './router'
+
+import 'primevue/resources/themes/aura-light-purple/theme.css'
 import 'primeicons/primeicons.css'
 import '/node_modules/primeflex/primeflex.css'
 import './style.css'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(PrimeVue, {})
+app.use(PrimeVue, { ripple: true })
+app.use(ToastService)
 app.use(router)
+
+app.component('Menubar', Menubar)
+app.component('Button', Button)
+app.component('Dialog', Dialog)
+app.component('InputText', InputText)
+app.component('Dropdown', Dropdown)
+app.component('InputNumber', InputNumber)
+app.component('Toast', Toast)
+app.component('IconField', IconField)
+app.component('InputIcon', InputIcon)
+app.component('Checkbox', Checkbox)
+app.component('Card', Card)
+app.component('Skeleton', Skeleton)
+app.component('Calendar', Calendar)
+
 app.mount('#app')
