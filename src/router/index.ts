31ea35b7a0b4ef7b63e8afdaf2a1fd86660a47b4
Diego Buriti <bs.diego97@gmail.com>
2024-06-10 17:19:09 -0300

    chore: Adding meta property for auth routers and page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/Home.vue'
 import LoginView from '@/views/Login.vue'
-import AccountDetail from '@/views/AccountDetail.vue'
+import AccountDetailView from '@/views/AccountDetail.vue'
+import AboutUsView from '@/views/AboutUs.vue'
 import { supabase } from '@/supababse'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/expense/:id', name: 'account-detail', component: AccountDetail }
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        title: 'Login',
+        auth: false
+      }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: 'Home',
+        auth: true
+      }
+    },
+    {
+      path: '/expense/:id',
+      name: 'account-detail',
+      component: AccountDetailView,
+      meta: {
+        title: 'Account Detail',
+        auth: true
+      }
+    },
+    {
+      path: '/about-us',
+      name: 'about-us',
+      component: AboutUsView,
+      meta: {
+        title: 'About Us',
+        auth: false
+      }
+    }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Expense Tracker | ${to.meta.title}`
+  next()
 })
 
 router.beforeEach(async (to, from, next) => {
   const { data } = await supabase.auth.getSession()
 
-  if (to.name !== 'login' && !data.session) next({ name: 'login' })
+  if (!!to.meta.auth && !data.session) next({ name: 'login' })
   else next()
 })
 
