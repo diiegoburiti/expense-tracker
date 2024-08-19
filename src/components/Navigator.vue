0bc645c4f1b0c8c6371a443dd0de2b38aae58eb2
Diego Buriti <bs.diego97@gmail.com>
2024-08-19 19:34:52 -0300

    feat: Add responsive behavior for navigator component

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,27 +1,27 @@
 <template>
   <div
-    class="flex justify-content-between px-4 py-1 border-1 border-solid surface-border bg-white"
+    class="flex align-items-center justify-content-between px-2 py-1 border-1 border-solid surface-border bg-white sm:px-5"
   >
-    <div class="flex align-items-center gap-2">
-      <i class="pi pi-money-bill" style="font-size: 2rem" />
-      <h3>Expen$e Tracker</h3>
+    <div class="flex align-items-center flex-1 gap-2">
+      <i class="pi pi-money-bill text-2xl sm:text-4xl" />
+      <h3 class="m-0 text-sm sm:text-base">Expen$e Tracker</h3>
     </div>
 
     <div class="">
       <ul class="flex gap-4">
         <router-link
-          class="no-underline text-900 font-bold"
+          class="no-underline text-900 font-bold text-sm sm:text-base"
           :to="{ name: 'home' }"
           >Home</router-link
         >
         <router-link
-          class="no-underline text-900 font-bold"
+          class="no-underline text-900 font-bold text-sm sm:text-base"
           :to="{ name: 'about-us' }"
           >About Us</router-link
         >
         <router-link
           v-if="!user"
-          class="no-underline text-900 font-bold"
+          class="no-underline text-900 font-bold text-sm sm:text-base"
           :to="{ name: 'login' }"
           >Login</router-link
         >
@@ -29,7 +29,7 @@
         <li
           v-if="user"
           @click="logout"
-          class="list-none cursor-pointer text-900 font-bold"
+          class="list-none cursor-pointer text-900 font-bold text-sm sm:text-base"
         >
           Log out
         </li>
