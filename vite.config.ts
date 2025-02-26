d38c4ee45ce9afed37c03586dbddbbd8cea25409
Diego Buriti <bs.diego97@gmail.com>
2025-02-26 20:57:28 -0300

    build: Configure manualChunks to group large dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue', 'vue-router']
+        }
+      }
     }
   }
 })
